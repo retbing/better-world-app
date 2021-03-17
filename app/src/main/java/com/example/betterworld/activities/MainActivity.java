a23146111285ae60a559087557a398aaedb5c089
Nurettin Resul Tanyıldızı <tanyildizi.resul@gmail.com>
2021-03-17 16:56:31 +0300

    update dependencies

--- app/src/main/java/com/example/betterworld/activities/MainActivity.java
+++ app/src/main/java/com/example/betterworld/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.betterworld;
+package com.example.betterworld.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.betterworld.R;
 
 public class MainActivity extends AppCompatActivity {
 
