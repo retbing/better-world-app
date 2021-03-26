e550276ab475d6af706f987d783ded0ff86f4214
Nurettin Resul Tanyıldızı <tanyildizi.resul@gmail.com>
2021-03-27 01:02:31 +0300

    fix

--- app/src/main/java/com/example/betterworld/activities/CharityFormActivity.java
+++ app/src/main/java/com/example/betterworld/activities/CharityFormActivity.java
@@ -6,28 +6,30 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 import com.example.betterworld.R;
+import com.example.betterworld.databinding.ActivityCharityFormBinding;
 import com.example.betterworld.databinding.ActivityChooseCategoryBinding;
 
 public class CharityFormActivity extends AppCompatActivity {
-    ActivityChooseCategoryBinding activityChooseCategoryBinding;
+    private ActivityCharityFormBinding activityCharityFormBinding;
     private int step;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activityChooseCategoryBinding = DataBindingUtil.setContentView(this, R.layout.activity_charity_form);
+        activityCharityFormBinding = DataBindingUtil.setContentView(this, R.layout.activity_charity_form);
+        _initComponents();
         step = 1;
     }
 
 
     private void _initComponents() {
-        activityChooseCategoryBinding.btnNext.setOnClickListener(view -> _nextStep());
+
     }
 
     private void _nextStep() {
         switch (step){
             case 1:
-                activityChooseCategoryBinding.btn
+
         }
     }
 
