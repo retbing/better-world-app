49843781c251490083506492b1b8833ea1e3640f
Nurettin Resul Tanyıldızı <tanyildiziresul@gmail.com>
2021-05-15 13:01:59 +0300

    Image uploading completed

--- app/src/main/java/com/example/betterworld/repositories/CharityRepository.java
+++ app/src/main/java/com/example/betterworld/repositories/CharityRepository.java
@@ -1,6 +1,7 @@
 package com.example.betterworld.repositories;
 
 
+import android.net.Uri;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -9,25 +10,49 @@ import androidx.lifecycle.MutableLiveData;
 import com.example.betterworld.models.Charity;
 import com.example.betterworld.models.DataOrException;
 import com.example.betterworld.models.User;
+import com.example.betterworld.utils.HelperClass;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.storage.StorageReference;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import dagger.hilt.android.scopes.ActivityScoped;
+
 import static com.example.betterworld.utils.Constants.CHARITIES_REF;
+import static com.example.betterworld.utils.Constants.CHARITIES_STORAGE_REF;
 import static com.example.betterworld.utils.HelperClass.logErrorMessage;
 
+@ActivityScoped
 public class CharityRepository {
 
     private CollectionReference charityCollection;
+    private StorageReference charityStorage;
 
     @Inject
-    public CharityRepository(@Named(CHARITIES_REF) CollectionReference charitiesRef) {
+    public CharityRepository(@Named(CHARITIES_REF) CollectionReference charitiesRef, @Named(CHARITIES_STORAGE_REF) StorageReference charityStorage) {
         this.charityCollection = charitiesRef;
+        this.charityStorage = charityStorage;
+    }
+
+    public MutableLiveData<DataOrException<String, Exception>> uploadImageToFirebaseStorage(Uri uri) {
+        MutableLiveData<DataOrException<String, Exception>> dataOrExceptionMutableLiveData = new MutableLiveData<>();
+        final String fileName = HelperClass.createUniqueImageName(uri);
+        charityStorage.child(fileName).putFile(uri).addOnCompleteListener(uploadTask -> {
+            DataOrException<String, Exception> dataOrException = new DataOrException<>();
+            if (uploadTask.isSuccessful()) {
+                dataOrException.data = fileName;
+            } else {
+                dataOrException.exception = uploadTask.getException();
+            }
+
+            dataOrExceptionMutableLiveData.setValue(dataOrException);
+        });
+        return dataOrExceptionMutableLiveData;
     }
 
 
--- app/src/main/java/com/example/betterworld/utils/HelperClass.java
+++ app/src/main/java/com/example/betterworld/utils/HelperClass.java
@@ -1,10 +1,21 @@
 package com.example.betterworld.utils;
 
+import android.net.Uri;
 import android.util.Log;
+
+import java.util.UUID;
+
 import static com.example.betterworld.utils.Constants.TAG;
 
 public class HelperClass {
-public static void logErrorMessage(String errorMessage) {
-            Log.d(TAG, errorMessage);
-        }
+    public static void logErrorMessage(String errorMessage) {
+        Log.d(TAG, errorMessage);
+    }
+
+    public static String createUniqueImageName(Uri uri) {
+        final String path = uri.getPath();
+        final String ext = path.substring(path.lastIndexOf("."));
+        final String name = UUID.randomUUID().toString();
+        return name + ext;
+    }
 }
